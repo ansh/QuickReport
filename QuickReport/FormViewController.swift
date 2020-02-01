f32f261beb10aba69a0f37c3d8fa47c230966848
Ansh Nanda <anshnanda10@gmail.com>
2020-02-01 17:23:27 -0500

    partly last screen done swift

--- QuickReport/FormViewController.swift
+++ QuickReport/FormViewController.swift
@@ -7,13 +7,89 @@
 //
 
 import UIKit
+import CoreLocation
 
 class FormViewController: UIViewController {
+    
+    var locManager = CLLocationManager()
+    var currentLocation: CLLocation!
 
+    @IBOutlet weak var problemText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locManager.requestWhenInUseAuthorization()
+        if(CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
+                CLLocationManager.authorizationStatus() ==  .authorizedAlways){
+
+              currentLocation = locManager.location
+              print(currentLocation.coordinate.latitude)
+              print(currentLocation.coordinate.longitude)
+
+        }
+        
+        getPlacemark(forLocation: currentLocation) {
+            (originPlacemark, error) in
+                print("we are here!")
+                if let err = error
+                {
+                    print(err)
+                }
+                else if let pm = originPlacemark
+                {
+                    var addressString : String = ""
+                    
+                    if pm.thoroughfare != nil {
+                        addressString = addressString + pm.thoroughfare! + "\n" // address
+                    }
+                    if pm.locality != nil {
+                        addressString = addressString + pm.locality! + ", " // city
+                    }
+                    if pm.country != nil {
+                        addressString = addressString + pm.administrativeArea! + ", " // state
+                    }
+                    if pm.postalCode != nil {
+                        addressString = addressString + pm.postalCode! + "\n" // zip code
+                    }
+                    if pm.postalCode != nil {
+                        addressString = addressString + pm.country! + " "
+                    }
+                    
+                    self.problemText.text = InitialViewController.chosenIssue
+                    
+                    print(InitialViewController.chosenIssue + "\n")
+                    print(pm.subLocality! + "\n")
+                    print(addressString)
+                }
+        }
 
         // Do any additional setup after loading the view.
+    }
+    
+    func getPlacemark(forLocation location: CLLocation, completionHandler: @escaping (CLPlacemark?, String?) -> ()) {
+        let geocoder = CLGeocoder()
+
+        geocoder.reverseGeocodeLocation(location, completionHandler: {
+            (placemarks, error) in
+
+            if let err = error {
+                completionHandler(nil, err.localizedDescription)
+            }
+            else if let placemarkArray = placemarks {
+                if let placemark = placemarkArray.first
+                {
+                    completionHandler(placemark, nil)
+                }
+                else
+                {
+                    completionHandler(nil, "Placemark was nil")
+                }
+            }
+            else {
+                completionHandler(nil, "Unknown error")
+            }
+        })
+
     }
     
 
@@ -28,3 +104,4 @@ class FormViewController: UIViewController {
     */
 
 }
+
