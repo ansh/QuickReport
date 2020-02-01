a99134b7c6464eb00c2df4263108cbc83a91c42b
Ansh Nanda <anshnanda10@gmail.com>
2020-02-01 14:41:34 -0500

    final view

---
+++ QuickReport/FormViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FormViewController.swift
+//  QuickReport
+//
+//  Created by Ansh Nanda on 2/1/20.
+//  Copyright © 2020 Rami Sbahi. All rights reserved.
+//
+
+import UIKit
+
+class FormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
